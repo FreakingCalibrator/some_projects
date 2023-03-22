19cf32380a65672c93d9cf6e021965905eb50e53
Pavel Filatov <50051417+FreakingCalibrator@users.noreply.github.com>
2023-03-22 14:12:45 +0300

    ex1
    Разработать программу solution, которая осуществляет поиск родительского PID текущего процесса в файловой системе proc и выводит найденное значение на консоль.  Результат, возвращаемый функций должен быть выведен отдельной строкой(должен оканчиваться символом перевода строки \n) в stdio.
    
    Пример вызова
    
    ./solution
    13221
    
    
    Представление решения
    
    Решение предоставляется в виде файла solution.c, содержащего исходный код Вашего приложения.
    
    Вывод
    
    Программа выводит в стандартный поток вывода число в отдельную строку(вывод должен оканчиваться символом перевода строки \n).

---
+++ solution.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+int main()
+{
+char str[5];
+int num=0;
+char c;
+int len;
+int fd=open("/proc/self/status", O_RDONLY);
+while ((read(fd,&str,4))>0)
+{
+str[4]=0;
+c=9;
+//printf("%s", str);
+
+if (strcmp(str,"PPid"))
+while (c!='\n'){
+read(fd,&c,1);
+//printf("%c ", c);
+}
+else
+{
+read(fd,&c,1);
+while (c!='\n'){
+read(fd,&c,1);
+if (c>47&&c<58)
+num=num*10+(int)c-48;
+}
+break;
+}
+}
+printf("%d\n",num);
+close(fd);
+return 0;
+} 
