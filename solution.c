5a081a23a92293b5b616a3b2f5ad2aca49b35cf3
Pavel Filatov <50051417+FreakingCalibrator@users.noreply.github.com>
2023-03-22 14:14:20 +0300

    ex2
    Разработать программу solution, которая осуществляет поиск и подсчет числа процессов с именем genenv в системе Linux на которой выполняется программа. 
    
    Пример вызова
    
    ./solution
    13
    
    
    Представление решения
    
    Решение предоставляется в виде файла solution.c, содержащего исходный код Вашего приложения.
    
    Вывод
    
    Программа выводит в стандартный поток вывода число в отдельную строку( вывод должен оканчиваться на символ перевода строки \n).

--- solution.c
+++ solution.c
@@ -4,36 +4,51 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <string.h>
-int main()
-{
-char str[5];
-int num=0;
-char c;
-int len;
-int fd=open("/proc/self/status", O_RDONLY);
-while ((read(fd,&str,4))>0)
-{
-str[4]=0;
-c=9;
-//printf("%s", str);
+#include <sys/types.h>
+#include <dirent.h>
 
-if (strcmp(str,"PPid"))
-while (c!='\n'){
-read(fd,&c,1);
-//printf("%c ", c);
-}
-else
+int main(int argc, char** argv)
 {
-read(fd,&c,1);
-while (c!='\n'){
-read(fd,&c,1);
-if (c>47&&c<58)
-num=num*10+(int)c-48;
+DIR* dir;
+char buff[41];
+char c;
+struct dirent *entry;
+int pid,i,count=0;
+dir=opendir("/proc/");
+if (!dir){
+	printf("Eroor in diropen proc");
+	exit(1);
 }
-break;
+while ((entry=readdir(dir))!=NULL)
+{
+	char str[30]="/proc/";
+	if (atoi(entry->d_name))
+	{
+		i=0;
+		strcat(str,entry->d_name);
+		strcat(str,"/status");
+		strcat(str,"\0");
+		//printf("%s-------",str);
+		pid=open(str,O_RDONLY);
+		for (i=0;i<6;i++)
+			read(pid,&c,1);
+		read(pid,&c,1);
+		for (i=0;i<30;i++)
+			buff[i]=0;
+		i=0;
+		while (c!='\n'){
+			//printf("%d",(int)c);
+			//strcat(buff,c);
+			buff[i++]=c;
+			read(pid,&c,1);
+		}
+		strcat(buff,"\0");
+		//buff[i++]=0;
+		//printf("%s\n",buff);
+		close(pid);
+		count+=!strcmp(buff,"genenv");
+	}
 }
-}
-printf("%d\n",num);
-close(fd);
+printf("%d\n",count);
 return 0;
-} 
+}
